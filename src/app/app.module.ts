c8d95c501fecac73543961f8bf8d45401ba27735
PHAM TIEN DANH(Pham Tien Danh - Steve) <danh100121@lgcns.com>
2024-12-27 15:31:58 +0700

    coding component product details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { DetailProductComponent } from './detail-product/detail-product.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    DetailProductComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [HomeComponent]
+  bootstrap: [
+    // HomeComponent
+    DetailProductComponent
+  
+  ]
 })
 export class AppModule { }
