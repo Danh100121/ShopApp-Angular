652d8d8fd45383881ed62f351774b9588108c614
PHAM TIEN DANH(Pham Tien Danh - Steve) <danh100121@lgcns.com>
2024-12-31 15:44:59 +0700

    coding component login register order order-confirm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,21 @@ import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { DetailProductComponent } from './detail-product/detail-product.component';
+import { OrderComponent } from './order/order.component';
+import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     HeaderComponent,
     FooterComponent,
-    DetailProductComponent
+    DetailProductComponent,
+    OrderComponent,
+    OrderConfirmComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
@@ -18,8 +26,12 @@ import { DetailProductComponent } from './detail-product/detail-product.componen
   providers: [],
   bootstrap: [
     // HomeComponent
-    DetailProductComponent
-  
+    // DetailProductComponent
+    // OrderComponent
+    // OrderConfirmComponent
+    // LoginComponent
+    RegisterComponent
+
   ]
 })
 export class AppModule { }
